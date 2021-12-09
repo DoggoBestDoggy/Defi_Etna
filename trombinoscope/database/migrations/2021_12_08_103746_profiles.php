35663ae7b706c30f38d43f98b8a4e24cca4a7123
bonhou_q <bonhou_q@etna-alternance.net>
2021-12-09 09:35:53 +0100

    Ajout d'un prototype des routes

---
+++ trombinoscope/database/migrations/2021_12_08_103746_profiles.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Profiles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!schema::hasTable('profiles')) {
+            Schema::create('profiles', function (Blueprint $table) {
+                $table->id();
+                $table->string('login', 45)->unique();
+                $table->string('firstname', 45);
+                $table->string('lastname', 45);
+                $table->string('email', 255)->unique();
+                $table->string('picture');
+                $table->foreignId('role_id');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
