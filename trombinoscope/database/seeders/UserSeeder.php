f4ba4a6371685ecc04912ee011112e7aa581d270
bernar_a <bernar_a@etna-alternance.net>
2021-12-10 10:58:20 -0500

    seeder

---
+++ trombinoscope/database/seeders/UserSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("profiles")->truncate();
+        DB::table('profiles')->insert([
+            [
+                'login' => 'samir',
+                'firstname' => 'Samir',
+                'lastname' => 'RINAZ',
+                'email' => 'samir@etna-alternance.net',
+                'picture' => 'https://auth.etna-alternance.net/api/users/samir/photo',
+                'work' => 'Directeur',
+                'role_id' => '0' //SuperAdmin
+            ],
+            [
+                'login' => 'deborah.cristiani',
+                'firstname' => 'Déborah',
+                'lastname' => 'CRISTIANI',
+                'email' => 'deborah.cristiani@etna-learning.fr',
+                'picture' => 'https://auth.etna-alternance.net/api/users/deborah.cristiani/photo',
+                'work' => 'Responsable RH',
+                'role_id' => '0' //SuperAdmin
+            ],
+            [
+                'login' => 'daime_a',
+                'firstname' => 'Adeline',
+                'lastname' => 'DAIME',
+                'email' => 'adeline.daime@etna-alternance.net',
+                'picture' => 'https://auth.etna-alternance.net/api/users/daime_a/photo',
+                'work' => 'Responsable RH',
+                'role_id' => '0' //SuperAdmin
+            ]
+        ]);
+    }
+}
